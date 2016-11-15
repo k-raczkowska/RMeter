740f6827526ef2cba429aac81964003e4c73014e
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-15 19:19:02 +0100

    add repos value to install.packages

--- tests/testthat/test_qwerty.r
+++ tests/testthat/test_qwerty.r
@@ -1,5 +1,5 @@
 library(testthat)
-install.packages("RMySQL")
+install.packages('RMySQL', repos='http://cran.us.r-project.org')
 
 #test_that("Otwieranie i zamykanie połączenia do bazy danych", {
 #  mydb = dbConnect(MySQL(), user = 'root', dbname = 'travistorrent', host = 'localhost')
