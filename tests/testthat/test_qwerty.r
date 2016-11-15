6526430946218ba527df22ba4631aac5b1e4733a
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-15 20:18:15 +0100

    test

--- tests/testthat/test_qwerty.r
+++ tests/testthat/test_qwerty.r
@@ -1,12 +1,11 @@
 library(testthat)
-install.packages(RMySQL, repos='http://cran.us.r-project.org')
-library(RMySQL)
+install.packages("RMySQL", repos='http://cran.us.r-project.org')
 
 
-#test_that("Otwieranie i zamykanie połączenia do bazy danych", {
-#  mydb = dbConnect(MySQL(), user = 'root', dbname = 'travistorrent', host = 'localhost')
-#  expect_true(inherits(mydb, "MySQLConnection"))
-#})
+test_that("Otwieranie i zamykanie połączenia do bazy danych", {
+  mydb = dbConnect(MySQL(), user = 'root', dbname = 'travistorrent', host = 'localhost')
+  expect_true(inherits(mydb, "MySQLConnection"))
+})
 
 test_that("Add", {
   expect_equal(2+2, 4)
