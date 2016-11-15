ddf4dbeb12112a4fcaa87a2b07c253c046ee7e2b
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-15 19:36:08 +0100

    fixing

--- tests/testthat/test_qwerty.r
+++ tests/testthat/test_qwerty.r
@@ -1,6 +1,5 @@
 library(testthat)
 install.packages('RMySQL', repos='http://cran.us.r-project.org')
-library(RMySQL)
 
 test_that("Otwieranie i zamykanie połączenia do bazy danych", {
   mydb = dbConnect(MySQL(), user = 'root', dbname = 'travistorrent', host = 'localhost')
