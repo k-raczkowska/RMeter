572ab65e9aaed20cde6b1930fbb301697ae39d06
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-15 19:58:39 +0100

    tests

--- tests/testthat/test_qwerty.r
+++ tests/testthat/test_qwerty.r
@@ -1,6 +1,6 @@
 library(testthat)
 install.packages('RMySQL', repos='http://cran.us.r-project.org')
-library(RMySQL)
+
 
 #test_that("Otwieranie i zamykanie połączenia do bazy danych", {
 #  mydb = dbConnect(MySQL(), user = 'root', dbname = 'travistorrent', host = 'localhost')
@@ -9,4 +9,13 @@ library(RMySQL)
 
 test_that("Add", {
   expect_equal(2+2, 4)
+})
+
+ip <- as.data.frame(installed.packages()[,c(1,3:4)])
+rownames(ip) <- NULL
+ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+print(ip, row.names=FALSE)
+
+test_that("Try", {
+  expect_equal(ip$Package, RMySQL)
 })
