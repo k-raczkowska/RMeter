a762b03f7ec6a8275a07513400b0422c73177cef
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-15 20:05:19 +0100

    fix

--- tests/testthat/test_qwerty.r
+++ tests/testthat/test_qwerty.r
@@ -1,5 +1,6 @@
 library(testthat)
-install.packages('RMySQL', repos='http://cran.us.r-project.org')
+#install.packages('RMySQL', repos='http://cran.us.r-project.org')
+library(RMySQL)
 
 
 #test_that("Otwieranie i zamykanie połączenia do bazy danych", {
@@ -9,13 +10,4 @@ install.packages('RMySQL', repos='http://cran.us.r-project.org')
 
 test_that("Add", {
   expect_equal(2+2, 4)
-})
-
-ip <- as.data.frame(installed.packages()[,c(1,3:4)])
-rownames(ip) <- NULL
-ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
-print(ip, row.names=FALSE)
-
-test_that("Try", {
-  expect_equal(ip$Package, RMySQL)
 })
