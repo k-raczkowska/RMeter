58643219146e57ab98c284b73320466361ec7544
k-raczkowska <karolina.raczkowska1@gmail.com>
2016-11-23 18:35:08 +0100

    authorize

--- R/github_connection.r
+++ R/github_connection.r
@@ -5,7 +5,8 @@ library(httpuv)
 install.packages('httr')
 library(httr)
 
-httr::oauth_endpoint("github")
+httr::oauth_endpoint("github", "authorize", "access_token",
+                     base_url = "https://github.com/login/oauth")
 myapp <- httr::oauth_app("github", "eb0e2d954e3c072e0e05", "59e73934db0cc7e51363d7c3b59c6daa35629b2b")
 github_token <- httr::oauth2.0_token(oauth_endpoints("github"), myapp)
 #req <- GET("https://api.github.com/rate_limit", config(token = github_token))
